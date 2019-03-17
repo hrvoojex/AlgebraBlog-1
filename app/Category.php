84dceff9006dea83966c8e08cf73d656a7b20306
Hrvoje <hrvooje@gmail.com>
2019-03-17 13:51:27 +0100

    Explicitly add pivot table name in Category model to belongsToMany() method

--- app/Category.php
+++ app/Category.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     public function posts(){
-        return $this->belongsToMany(Post::class);
+        return $this->belongsToMany('App\Post', 'post_category');
     }
 
     public function getRouteKeyName(){
